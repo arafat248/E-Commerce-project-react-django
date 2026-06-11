2b3a8418b8f1927d92bc783c15170947f818ec9a
arafat248 <aab.arafat248@gmail.com>
2026-06-11 17:07:44 +0600

    Build Frontend site and connecting to Backend site

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+function App(){
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/api/')
+      .then(response => response.json())
+      .then(data => setMessage(data.message))
+      .catch(error => console.error('Error fetching message:', error));
+  }, []);
+  return (
+    <div>
+      <h1>Message from Backend:</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
+export default App;
